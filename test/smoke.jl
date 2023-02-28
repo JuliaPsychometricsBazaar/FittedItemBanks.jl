8ec4b2831ebb92eabd7cc8a64098cc3f60498a57
Frankie Robertson <frankie@robertson.name>
2023-02-28 18:38:39 +0200

    Expand smoke tests

--- test/smoke.jl
+++ test/smoke.jl
@@ -1,14 +1,44 @@
-using PsychometricsBazzarBase.Integrators
-using PsychometricsBazzarBase.Optimizers
-using Optim
+using FittedItemBanks
+using FittedItemBanks.DummyData
 
+function dummy(spec::SimpleItemBankSpec{DomainT <: OneDimContinuousDomain})
+    dummy_item_bank(
+        Random.default_rng(42),
+        spec;
+        num_questions=4
+    )
+end
 
-const things = [
-    OneDimOptimOptimizer(-6.0, 6.0, NelderMead()),
-    QuadGKIntegrator(-6, 6, 5),
-    FixedGKIntegrator(-6, 6, 80),
-]
+function dummy(spec::SimpleItemBankSpec{DomainT <: VectorContinuousDomain})
+    dummy_item_bank(
+        Random.default_rng(42),
+        spec;
+        num_questions=4,
+        dims=2
+    )
+end
 
-for thing in things
-    thing(x -> x)
+function test_item_bank(item_bank)
+    rng = Random.default_rng(42)
+    for idx in eachindex(item_bank)
+        if item_bank.domain isa OneDimContinuousDomain
+            theta = rng.randn()
+        else
+            theta = rng.randn(domdims(item_bank))
+        end
+        resp = resp_vec(ItemResponse(item_bank, idx), theta)
+        @test isapprox(sum(resp), 1.0)
+        if item_bank.response isa BooleanResponse
+            @test length(resp) == 2
+        end
+    end
+end
+
+for model in [StdModel2PL(), StdModel3PL(), StdModel4PL()]
+    for domain in [VectorContinuousDomain(), OneDimContinuousDomain()]
+        for response in [BooleanResponse(), MultinomialResponse(3)]
+            item_bank = dummy(SimpleItemBankSpec(model, domain, response))
+            test_item_bank(item_bank)
+        end
+    end
 end
