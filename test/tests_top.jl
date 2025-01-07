84b9619ed6276b87053d15c712e77018f22f0f07
Frankie Robertson <frankie@robertson.name>
2025-01-07 13:31:03 +0200

    Add test for struct concreteness

--- test/tests_top.jl
+++ test/tests_top.jl
@@ -8,6 +8,12 @@ end
     include("./jet.jl")
 end
 
+@testset "structs" begin
+    using CheckConcreteStructs
+    using FittedItemBanks: FittedItemBanks
+    @test all_concrete(FittedItemBanks)
+end
+
 @testset "basic" begin
     include("./basic.jl")
 end
