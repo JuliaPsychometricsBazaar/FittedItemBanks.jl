0341cd812e931ff93c3e77626fe9e2331ecae657
Frankie Robertson <frankie@robertson.name>
2025-03-20 11:57:02 +0200

    Script test Project.toml

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,5 +1,6 @@
 using Aqua
 using FittedItemBanks
 
-Aqua.test_all(FittedItemBanks, ambiguities = false)
+Aqua.test_all(FittedItemBanks, ambiguities = false, deps_compat = false)
 Aqua.test_ambiguities([FittedItemBanks])
+Aqua.test_deps_compat(FittedItemBanks; check_extras = false)
