314f7242d9f5080fca4e5550915074ff99a355dd
Frankie Robertson <frankie@robertson.name>
2025-03-23 00:38:09 +0200

    Switch back to Test.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,23 @@
-using XUnit
+using Test
 
-runtests("tests_top.jl", ARGS...)
+@testset "aqua" begin
+    include("./aqua.jl")
+end
+
+@testset "jet" begin
+    include("./jet.jl")
+end
+
+@testset "structs" begin
+    using CheckConcreteStructs
+    using FittedItemBanks: FittedItemBanks
+    @test all_concrete(FittedItemBanks)
+end
+
+@testset "basic" begin
+    include("./basic.jl")
+end
+
+@testset "format" begin
+    include("./format.jl")
+end
